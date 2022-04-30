12246f0990561da45a92550088ef6cfc29d08189
Artyom Ushkov <80984319+Artyomushkov@users.noreply.github.com>
2022-04-30 19:39:39 +0300

    unused

--- madina/YoupiBanane/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $resSTDIN=fopen("php://stdin","r");
-	$strChar = stream_get_contents($resSTDIN);
-	echo("Content-Type: text/html; charset=utf-8\r\n\r\n");
-	echo(strlen($strChar));
-//    echo($strChar);
-    fclose($resSTDIN);
-?>
--- madina/data/web/session/set_color.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_POST['new_color']))
-{
-	session_start();
-	$_SESSION['new_color'] = $_POST['new_color'];
-}
-else
-	http_response_code(404);
-?>
